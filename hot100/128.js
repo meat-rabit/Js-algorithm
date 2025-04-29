b44a215ba20f846d93ac6b97a90612180469a472
rabbit <chengpeiwww@163.com>
2025-04-29 20:06:03 +0800

    128.js的更新 // 1. 128.js的更新 // 2. 128.js的更新 // 3. 128.js的更新 // 4. 128.js的更新 // 5. 128.js的更新 // 6. 128.js的更新 // 7. 128.js的更新 // 8. 128.js的更新 // 9. 128.js的更新 // 10. 128.js的更新

--- hot100/128.js
+++ hot100/128.js
@@ -50,27 +50,27 @@ console.log(longestConsecutive(nums))
 // 官解
 const longestConsecutive1 = (nums) => {
     // 使用哈希集合去重
-    const numSet = new Set(nums);
-    let maxLength = 0;
+    const numSet = new Set(nums) // 哈希集合，去重 + O(1) 查询
+    let maxLength = 0
 
     for (const num of numSet) {
         // 检查是否是序列的起点
-        if (!numSet.has(num - 1)) {
-            let currentLength = 1; // 当前序列长度初始化为1
-            let currentNum = num;
+        if (!numSet.has(num - 1)) { //判断 num 是否是序列起点：如果 num - 1 不在集合中，说明 num 是一个新的连续序列的开始
+            let currentLength = 1 // 当前序列长度初始化为1
+            let currentNum = num
 
             // 向后扩展序列
-            while (numSet.has(currentNum + 1)) {
-                currentNum += 1;
-                currentLength += 1;
+            while (numSet.has(currentNum + 1)) { //连续的定义（数值差为1）是通过 currentNum + 1 来实现的 has()是set的内置函数
+                currentNum += 1
+                currentLength += 1
             }
 
             // 更新最大长度
-            maxLength = Math.max(maxLength, currentLength);
+            maxLength = Math.max(maxLength, currentLength)
         }
     }
 
-    return maxLength;
-};
+    return maxLength
+}
 nums = [0,3,7,2,5,8,4,6,0,1]
 console.log(longestConsecutive1(nums))
--- hot100/哈希.js
+++ hot100/哈希.js
@@ -1,2 +1,3 @@
 // 1 128 49所用的基本思想是哈希，所以这里讨论一下哈希算法
-// 具体内容看我博客去
+// 具体内容看我博客去：https://blog.csdn.net/nailixiaomianbao/article/details/147618456?sharetype=blogdetail&sharerId=147618456&sharerefer=PC&sharesource=nailixiaomianbao&spm=1011.2480.3001.8118
+
